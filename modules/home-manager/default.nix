9bcb1d8568df1a5a9be84fe626a316d513ecb2f3
Liam Vanderpoel <liam@superlinear.com>
2024-11-18 16:39:32 +0100

    Update home-manager packages

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -4,31 +4,73 @@
   ...
 }: {
   # Don't change this when you change package input. Leave it alone.
-  home.stateVersion = "24.05";
-  # specify my home-manager configs
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-    curl
-    less
-    nvim.packages."aarch64-darwin".default
-  ];
-  home.sessionVariables = {
-    PAGER = "less";
-    CLICLOLOR = 1;
-    EDITOR = "nvim";
+  home = {
+    stateVersion = "24.05";
+    # specify my home-manager configs
+    packages = with pkgs; [
+      curl # Command line tool for transferring data with URLs
+      less # Terminal pager for viewing file contents
+      tree # Directory listing in tree-like format
+      oh-my-zsh # Framework for managing zsh configuration
+      zsh-powerlevel10k # Modern, fast zsh theme
+      # poetry
+      ripgrep # Fast grep alternative written in Rust
+      fzf # Command-line fuzzy finder
+      bat # Cat clone with syntax highlighting and git integration
+      nvim.packages."aarch64-darwin".default
+    ];
+    sessionVariables = {
+      PAGER = "less";
+      CLICLOLOR = 1;
+      EDITOR = "nvim";
+    };
   };
-  programs.bat.enable = true;
-  programs.bat.config.theme = "TwoDark";
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = true;
-  programs.eza.enable = true;
-  programs.git.enable = true;
-  programs.zsh.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.autosuggestion.enable = true;
-  programs.zsh.syntaxHighlighting.enable = true;
-  programs.zsh.shellAliases = {
-    ls = "ls --color=auto -F";
+  programs = {
+    bat = {
+      enable = true;
+      config.theme = "TwoDark";
+    };
+
+    git = {
+      enable = true;
+      extraConfig = {
+        init.defaultBranch = "master";
+        user.name = "Liam Vanderpoel";
+        user.email = "liam@superlinear.com";
+        merge.tool = "opendiff";
+        diff.tool = "opendiff";
+        difftool.prompt = false;
+        difftool."opendiff" = ''cmd = /usr/bin/opendiff "$LOCAL" "$REMOTE" -merge "$MERGED" | cat'';
+      };
+    };
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+      ];
+
+      shellAliases = {
+        ls = "ls --color=auto -F";
+        g = "git";
+        nixnix = "nix flake update; darwin-rebuild switch --flake .#";
+        nixswitch = "darwin-rebuild switch --flake ~/nix-config.#";
+        nixup = "pushd ~/nix-config; nix flake update; nixswitch; popd";
+      };
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = ["git" "docker"];
+      };
+    };
   };
 }
