801d4055dce791d00ed3322519ed00eaab1c8094
Liam Vanderpoel <vanderpoel.liam@gmail.com>
2026-01-15 09:36:46 +0100

    Add hardware configuration for Hyperion

--- modules/nixos/hyperion/hardware.nix
+++ modules/nixos/hyperion/hardware.nix
@@ -1,14 +1,31 @@
-{ lib, modulesPath, pkgs, config, ... }:
+{ lib, modulesPath, pkgs, ... }:
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Kernel modules - adjust after hardware scan
   boot.initrd.availableKernelModules = [
     "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"
   ];
+
+  # Intel N100 iGPU support
+  boot.kernelParams = [ "i915.force_probe=a721" ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+    ];
+  };
+
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+
+  # ConBee Zigbee coordinator
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", SYMLINK+="zigbee"
+  '';
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
