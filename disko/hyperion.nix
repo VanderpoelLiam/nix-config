0496fd60d5ead1c3a3b59b9ede377e3b4294d1d7
Liam Vanderpoel <vanderpoel.liam@gmail.com>
2026-02-01 13:16:44 +0100

    Update Hyperion disk device from /dev/sda to /dev/nvme0n1

--- disko/hyperion.nix
+++ disko/hyperion.nix
@@ -2,7 +2,7 @@
   disko.devices = {
     disk.main = {
       type = "disk";
-      device = "/dev/sda";
+      device = "/dev/nvme0n1";
       content = {
         type = "gpt";
         partitions = {
