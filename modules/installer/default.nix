74592a551de37b1ba305b5cab6070790ea01d5f4
Liam Vanderpoel <vanderpoel.liam@gmail.com>
2026-02-03 21:23:46 +0100

    Simplify installer

--- modules/installer/default.nix
+++ modules/installer/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 let
   userConfig = import ../../user-config.nix;
@@ -9,36 +9,23 @@ in
     "${pkgs.path}/nixos/modules/installer/cd-dvd/channel.nix"
   ];
 
-  # Enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Use beta cache for faster downloads
   # See this discussion: https://discourse.nixos.org/t/anyone-get-really-slow-downloads-from-cache-nixos-org/73941
   nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://aseipp-nix-cache.global.ssl.fastly.net"
     ];
   };
-
-  # Locale
-  i18n.supportedLocales = [ "en_US.UTF-8/UTF-8" ];
-  i18n.defaultLocale = "en_US.UTF-8";
-  time.timeZone = userConfig.global.timezone;
-
-  # Passwordless sudo for wheel group
+  
   security.sudo.wheelNeedsPassword = false;
 
-  # Create user with SSH key
   users.users.${userConfig.global.username} = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [ userConfig.global.sshPublicKey ];
   };
 
-  programs.zsh.enable = true;
-
-  # SSH configuration
   services.openssh = {
     enable = true;
     settings = {
@@ -47,7 +34,6 @@ in
     };
   };
 
-  # Essential packages for installation
   environment.systemPackages = with pkgs; [
     git
     vim
@@ -55,6 +41,5 @@ in
     wget
     rsync
   ];
-
   system.stateVersion = "25.11";
 }
